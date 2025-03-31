4b2d849d3c75317a3b6c90dd3420542fc9cbf5b9
Sai Vikram Srigada <saivikramsrigada@sai-srigada-macbook-pro---mlygvwr49d.tail9ad93.ts.net>
2025-03-31 16:34:16 +0530

    Fix Homebrew formula for k3s-prereqs

--- Formula/k3s-prereqs.rb
+++ Formula/k3s-prereqs.rb
@@ -1,21 +1,19 @@
 class K3sPrereqs < Formula
-    desc "Installs prerequisites for K3s setup"
-    homepage "https://github.com/airbytehq/support-k3s-lab"
-    version "1.0.0"
-  
-    depends_on "yq"
-    depends_on "hashicorp/tap/hashicorp-vagrant"
-  
-    def install
-      # Install Vagrant Hostmanager plugin
-      ohai "Installing vagrant-hostmanager plugin..."
-      system "vagrant", "plugin", "install", "vagrant-hostmanager"
-    end
-  
-    test do
-      system "yq", "--version"
-      system "vagrant", "--version"
-      assert_match /vagrant-hostmanager/, shell_output("vagrant plugin list")
-    end
+  desc "Installs prerequisites for K3s setup"
+  homepage "https://github.com/airbytehq/support-k3s-lab"
+  version "1.0.0"
+
+  depends_on "yq"
+  depends_on "hashicorp/tap/hashicorp-vagrant"
+
+  def install
+    ohai "Installing vagrant-hostmanager plugin..."
+    system "vagrant", "plugin", "install", "vagrant-hostmanager"
   end
-  
+
+  test do
+    system "yq", "--version"
+    system "vagrant", "--version"
+    assert_match /vagrant-hostmanager/, shell_output("vagrant plugin list")
+  end
+end
