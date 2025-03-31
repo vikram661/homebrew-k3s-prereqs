282151d90749c3fe1e7b115581d3b96d215bb25c
Sai Vikram Srigada <saivikramsrigada@sai-srigada-macbook-pro---mlygvwr49d.tail9ad93.ts.net>
2025-03-31 17:13:43 +0530

    Fix Vagrant installation check

--- Formula/k3s-prereqs.rb
+++ Formula/k3s-prereqs.rb
@@ -2,20 +2,22 @@ class K3sPrereqs < Formula
   desc "Installs prerequisites for K3s setup"
   homepage "https://github.com/airbytehq/support-k3s-lab"
   version "1.0.0"
-  sha256 "761f4c233486f04351e2f23c723285836bdaf62290c5a48badd996815e702d8f" # Dummy SHA for an empty script
 
   url "https://raw.githubusercontent.com/vikram661/homebrew-k3s-prereqs/main/k3s-prereqs.sh"
+  sha256 "761f4c233486f04351e2f23c723285836bdaf62290c5a48badd996815e702d8f" # Update with correct hash
 
   depends_on "yq"
 
   def install
     bin.install "k3s-prereqs.sh"
 
-    # Install Vagrant (since it's a Cask, we use system calls)
-    ohai "Installing Vagrant..."
-    system "brew", "install", "--cask", "vagrant"
+    unless system("vagrant --version")
+      ohai "Installing Vagrant..."
+      system "brew", "install", "--cask", "vagrant" or odie "Failed to install Vagrant!"
+    else
+      ohai "Vagrant is already installed, skipping installation."
+    end
 
-    # Install Vagrant Hostmanager plugin
     ohai "Installing vagrant-hostmanager plugin..."
     system "vagrant", "plugin", "install", "vagrant-hostmanager"
   end
