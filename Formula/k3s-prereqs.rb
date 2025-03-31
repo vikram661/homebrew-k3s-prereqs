0be697c930baec36cd9fbbf5c72337adc8a39046
Sai Vikram Srigada <saivikramsrigada@sai-srigada-macbook-pro---mlygvwr49d.tail9ad93.ts.net>
2025-03-31 16:48:30 +0530

    Update SHA256 hash for k3s-prereqs.sh

--- Formula/k3s-prereqs.rb
+++ Formula/k3s-prereqs.rb
@@ -2,7 +2,7 @@ class K3sPrereqs < Formula
   desc "Installs prerequisites for K3s setup"
   homepage "https://github.com/airbytehq/support-k3s-lab"
   version "1.0.0"
-  sha256 "d41d8cd98f00b204e9800998ecf8427e" # Dummy SHA for an empty script
+  sha256 "761f4c233486f04351e2f23c723285836bdaf62290c5a48badd996815e702d8f" # Dummy SHA for an empty script
 
   url "https://raw.githubusercontent.com/vikram661/homebrew-k3s-prereqs/main/k3s-prereqs.sh"
 
