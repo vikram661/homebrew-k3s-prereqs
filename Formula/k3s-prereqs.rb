b3be06475b7e2e9ab056da8ce74c88691f31399a
Sai Vikram Srigada <saivikramsrigada@sai-srigada-macbook-pro---mlygvwr49d.tail9ad93.ts.net>
2025-03-31 16:24:52 +0530

    Fix syntax error in test block

--- Formula/k3s-prereqs.rb
+++ Formula/k3s-prereqs.rb
@@ -1,34 +1,21 @@
 class K3sPrereqs < Formula
     desc "Installs prerequisites for K3s setup"
-    homepage "https://github.com/airbytehq/support-k3s-lab" # Update with your project's homepage
+    homepage "https://github.com/airbytehq/support-k3s-lab"
     version "1.0.0"
   
-    def install
-      # Ensure Homebrew is installed (informational, not enforced in formula)
-      ohai "Checking for Homebrew..."
-      unless which("brew")
-        odie "Homebrew is not installed. Please install it first: https://brew.sh/"
-      end
+    depends_on "yq"
+    depends_on "hashicorp/tap/hashicorp-vagrant"
   
-      # Install yq
-      ohai "Installing yq..."
-      system "brew", "install", "yq"
-      
-      # Install Vagrant via HashiCorp tap
-      ohai "Adding HashiCorp tap and installing Vagrant..."
-      system "brew", "tap", "hashicorp/tap"
-      system "brew", "install", "hashicorp/tap/hashicorp-vagrant"
-      
+    def install
       # Install Vagrant Hostmanager plugin
       ohai "Installing vagrant-hostmanager plugin..."
       system "vagrant", "plugin", "install", "vagrant-hostmanager"
     end
   
     test do
-      # Verify installations
       system "yq", "--version"
       system "vagrant", "--version"
-      system "vagrant", "plugin", "list" | grep "vagrant-hostmanager"
+      assert_match /vagrant-hostmanager/, shell_output("vagrant plugin list")
     end
   end
   
